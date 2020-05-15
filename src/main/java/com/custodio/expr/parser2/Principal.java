e7187eec1c729bae57c4a90322d4d7126a0ead5c
celson custodio <celsoncustodio@outlook.com>
2020-05-14 23:14:16 -0300

    .

---
+++ src/main/java/com/custodio/expr/parser2/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.custodio.expr.parser2;
+import java.io.IOException;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+/**
+ *
+ * @author celson
+ */
+public class Principal {
+    
+     public static void main(String args[]) throws IOException {
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        ExpressoesLexer lexer = new ExpressoesLexer(cs);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ExpressoesParser parser = new ExpressoesParser(tokens);
+        ExpressoesParser.ProgramaContext arvore = parser.programa();
+        Calculador c = new Calculador();
+        double val = c.visitPrograma(arvore);
+        System.out.println("Valor2 = " + val);
+    }
+}
